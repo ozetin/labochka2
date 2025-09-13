fed92c3946beb95b9689251bf4a9d140266921aa
Вадим <vadimkairov20@gmail.com>
2025-09-13 18:39:35 +0300

    Редактирован файл individual_hard.py

--- individual_hard.py
+++ individual_hard.py
@@ -1,2 +1,12 @@
-
-num3 = int(input("Enter a three-digit number"))
+numb2 = [0]
+numb3 = []
+total = [0,0,0]
+for i in input("Enter a two-digit number\n"):
+    numb2.append(int(i))
+for i in input("Enter a three-digit number\n"):
+    numb3.append(int(i))
+for i in range(2,-1,-1):
+    total[i] += (numb2[i]+numb3[i])%10
+    total[i-1] += (numb2[i]+numb3[i])//10
+for i in total:
+    print(i)
