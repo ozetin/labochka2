522a293441d7446cedf8682c570149a1a74029de
Вадим <vadimkairov20@gmail.com>
2025-09-16 19:13:31 +0300

    Корректировка индивидуального задания

--- individual.py
+++ individual.py
@@ -2,7 +2,7 @@ assortment = ["sweets","cookies","apples"]
 price = []
 total = 0
 for i in range(3):
-    price.append(int(input(f"Enter the price of {assortment[i]}")))
+    price.append(int(input(f"Enter the price of {assortment[i]}\n")))
 for i in range(3):
-    total += int(input(f"How many {assortment[i]} did you buy?")) * price[i]
+    total += int(input(f"How many {assortment[i]} did you buy?\n")) * price[i]
 print(f"Your total is: {total}")
