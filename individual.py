6bcdb2084505a758b52ee1e391fb9806a098b77e
Вадим <vadimkairov20@gmail.com>
2025-10-05 22:40:58 +0300

    изменён файл individual.py

--- individual.py
+++ individual.py
@@ -1,8 +1,11 @@
-assortment = ["sweets","cookies","apples"]
-price = []
+sweets = int(input("Enter the price of sweets: "))
+cookies = int(input("Enter the price of cookies: "))
+apples = int(input("Enter the price of apples: "))
+sweets_amount = int(input("How many sweets did you buy? "))
+cookies_amount = int(input("How many cookies did you buy? "))
+apples_amount = int(input("How many apples? "))
 total = 0
-for i in range(3):
-    price.append(int(input(f"Enter the price of {assortment[i]}\n")))
-for i in range(3):
-    total += int(input(f"How many {assortment[i]} did you buy?\n")) * price[i]
+total += sweets * sweets_amount
+total += cookies * cookies_amount
+total += apples * apples_amount
 print(f"Your total is: {total}")
